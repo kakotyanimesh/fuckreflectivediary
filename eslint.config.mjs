8f3921cf633b8fdcdf6159b456168d208ce7747a
animesh <phb21013@tezu.ac.in>
2025-05-08 23:17:31 +0530

    eslint disable for generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores : [
+      'node_modules/',
+      '.next/',
+      '.out/',
+      'src/generated/**'
+
+    ]
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
